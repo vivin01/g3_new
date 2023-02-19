2102c34d2a0c9878d1f88c6d19d9c5a8b25987c3
vivin01 <vivinmevada01@gmail.com>
2023-02-19 12:10:59 +0530

    third 456

--- app/src/main/java/com/vivin/myproject/UserImagesList.java
+++ app/src/main/java/com/vivin/myproject/UserImagesList.java
@@ -38,7 +38,7 @@ public class UserImagesList extends AppCompatActivity {
         Toast.makeText(this, "vivin", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+
 
         ArrayList<String> list = new ArrayList<>();
         ArrayAdapter adapter = new ArrayAdapter<String>(UserImagesList.this,R.layout.activity_list_item, list);
